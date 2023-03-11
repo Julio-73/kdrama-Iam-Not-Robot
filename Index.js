420d020ea60691888e0a09d5b6f7d4fdccd18fc1
julio-cesar <juliocesarquispegarrido@gmail.com>
2023-03-10 20:51:28 -0500

    Cambios

---
+++ Index.js
@@ -0,0 +1,13 @@
+/* ======Scroll ====*/
+
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 1000,
+    delay: 200
+
+
+});
+
+ScrollReveal().reveal('.contenedor', { origin: 'bottom'});
+
