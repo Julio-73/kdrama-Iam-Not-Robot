b7b5fdd37a7df18219db7bcdc189079a1420a756
julio-cesar <112985882+Julio-73@users.noreply.github.com>
2023-06-11 00:59:42 -0500

    ---- new commit -----

--- Index.js
+++ Index.js
@@ -1,30 +1,26 @@
 /* ======Scroll ====*/
 
-ScrollReveal({ 
-    reset: true,
-    distance: '80px',
-    duration: 1000,
-    delay: 200
-
-
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 1000,
+  delay: 200,
 });
 
-ScrollReveal().reveal('.contenedor', { origin: 'bottom'});
+ScrollReveal().reveal(".contenedor", { origin: "bottom" });
 
+ScrollReveal().reveal(".contenedor h1", { origin: "left" });
 
-ScrollReveal().reveal('.contenedor h1', { origin: 'left'});
+ScrollReveal().reveal(".contenedor p", { origin: "right" });
 
-ScrollReveal().reveal('.contenedor p', { origin: 'right'});
+ScrollReveal().reveal(".amores h3", { origin: "left" });
 
-ScrollReveal().reveal('.amores h3', { origin: 'left'});
+ScrollReveal().reveal(".amores li", { origin: "right" });
 
-ScrollReveal().reveal('.amores li', { origin: 'right'});
+ScrollReveal().reveal(".reparto h3", { origin: "left" });
 
-ScrollReveal().reveal('.reparto h3', { origin: 'left'});
+ScrollReveal().reveal(".reparto li", { origin: "right" });
 
-ScrollReveal().reveal('.reparto li', { origin: 'right'});
+ScrollReveal().reveal(" h2", { origin: "bottom" });
 
-ScrollReveal().reveal(' h2', { origin: 'bottom'});
-
-
-ScrollReveal().reveal(' h3', { origin: 'left'});
+ScrollReveal().reveal(" h3", { origin: "left" });
